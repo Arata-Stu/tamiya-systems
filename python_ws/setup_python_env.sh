7af282361b4b6e1f76c28190f773ec17e45bcfbf
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-03-14 21:29:44 +0900

    feat: edit pyenv setup scripts

--- python_ws/setup_python_env.sh
+++ python_ws/setup_python_env.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+set -e
+
 cd /python_ws/
 pip3 install -r requirements.txt
 pip3 install -e f1tenth_gym_jax
 
-pip uninstall -y jax jaxlib && \
-pip install --no-cache-dir "jax[cuda12]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html 
+pip3 uninstall -y jax jaxlib
+pip3 install --no-cache-dir "jax[cuda12]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.10/dist-packages:/home/admin/.local/lib/python3.10/site-packages
+python3 -c "import jax; print('Devices:', jax.devices())"
