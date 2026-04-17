62079cecc06342da866ac635595470d6a1d74939
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-04-17 12:15:35 +0900

    fix: update parameter file paths for ftg_controller and adjust tmux launch command

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -7,7 +7,7 @@ ROS_WS_PATH="${ISAAC_ROS_WS}"                       # ROS 2ワークスペース
 SETUP_SCRIPT="source /workspaces/install/setup.bash" # setup.bashへのフルパスを指定
 
 # --- 実行するコマンド群 ---
-CMD_BASE="ros2 launch system_launch system.launch.xml vslam:=true use_camera:=true use_lidar:=true use_ftg:=true localization:=false record:=false"  # 左ペインで実行するROS 2起動コマンド
+CMD_BASE="ros2 launch system_launch system.launch.xml vslam:=false use_camera:=true use_lidar:=true use_ftg:=false localization:=false record:=false"  # 左ペインで実行するROS 2起動コマンド
 CMD_MONITOR="bash /scripts/monitor.sh"
 
 # --- セッション名の決定 ---
