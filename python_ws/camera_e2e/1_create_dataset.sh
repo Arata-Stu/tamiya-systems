eab0aef53e262435d288d8016b7eec93e53e4530
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-04-24 11:22:06 +0900

    fix: update image topic in dataset creation script and adjust lidar parameters

--- python_ws/camera_e2e/1_create_dataset.sh
+++ python_ws/camera_e2e/1_create_dataset.sh
@@ -29,7 +29,7 @@ show_help() {
 
 BASE_DIR=""
 OUTDIR=""
-IMAGE_TOPIC="/camera/left/image_raw"
+IMAGE_TOPIC="/realsense2_camera/color/image_raw"
 CMD_TOPIC="/jetracer/cmd_drive"
 IMAGE_STORAGE="npy"
 WORKERS=""
