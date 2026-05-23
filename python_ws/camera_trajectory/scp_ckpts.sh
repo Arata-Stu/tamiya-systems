beebf15b738bc19eb07e6aa800df614729584094
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-05-24 05:41:46 +0900

    fix: update remote IP addresses for checkpoint transfer scripts

--- python_ws/camera_trajectory/scp_ckpts.sh
+++ python_ws/camera_trajectory/scp_ckpts.sh
@@ -22,7 +22,7 @@ fi
 # ==========================================
 DEFAULT_BASE_DIR="./ckpts/train/"
 DEFAULT_REMOTE_USER="tamiya"
-DEFAULT_REMOTE_IPS=("10.42.0.1" "192.168.55.1" "192.168.11.98")
+DEFAULT_REMOTE_IPS=("10.42.0.1" "192.168.55.1" "192.168.11.190")
 DEFAULT_REMOTE_DIR="/home/tamiya/workspaces/tamiya-systems/python_ws/ckpts/pilotnet_trajectory/"
 LOCAL_LIST_MAX_DEPTH=${LOCAL_LIST_MAX_DEPTH:-4}
 # ==========================================
