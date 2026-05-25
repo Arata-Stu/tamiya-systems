7a0dffe2fb88bfcc1937019bf149a60c6e0edf50
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2026-05-26 01:35:08 +0900

    feat

--- scripts/lib/tmux/layouts.sh
+++ scripts/lib/tmux/layouts.sh
@@ -82,7 +82,6 @@ create_e2e_layout() {
 create_lidar_e2e_layout() {
   reset_panes
   add_pane "$WINDOW_E2E" "$WORK_DIR" "$ROS_SETUP" "$CMD_LIDAR_E2E_RUN"
-  add_pane "$WINDOW_E2E" "$WORK_DIR" "$ROS_SETUP" "$CMD_LOCALIZATION_TRIGGER"
   add_pane "$WINDOW_VISUAL" "$WORK_DIR" "$ROS_SETUP" "$CMD_DASHBOARD_VSLAM_HD"
   add_pane "$WINDOW_VISUAL" "$WORK_DIR" "$ROS_SETUP" "$RVIZ_LOCALIZATION_EVAL"
   add_pane "$WINDOW_DATA" "$WORK_DIR" "$ROS_SETUP" "$CMD_RECORD_START"
